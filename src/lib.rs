eabafc01f05ec39746e1fe9b1e4b2c96e6a64d78
Seb Glazebrook <sebastian.glazebrook@redbubble.com>
2016-03-08 22:53:29 +1100

    Initial simple filtering

--- src/lib.rs
+++ src/lib.rs
@@ -1,6 +1,98 @@
-#[cfg(test)]
-mod test {
-    #[test]
-    fn it_works() {
-    }
+extern crate directory_scanner;
+extern crate regex;
+
+use directory_scanner::{ScannerBuilder, Directory};
+use std::path::PathBuf;
+use regex::Regex;
+
+struct FilteredDirectory<'b> {
+    directory: &'b Directory,
+    matches: Vec<String>, // TODO this should be a collection of references/pointers to paths in the directory
 }
+
+impl<'b> FilteredDirectory<'b> {
+
+    pub fn len(&self) -> usize {
+        self.matches.len()
+    }
+
+}
+
+struct SimpleFilter<'a> {
+    directory: &'a Directory,
+    filter_string: &'a str,
+}
+
+impl<'a> SimpleFilter<'a> {
+
+    pub fn new(directory: &'a Directory, filter_string: &'a str) -> Self {
+        SimpleFilter {
+            directory: directory,
+            filter_string: filter_string
+        }
+    }
+
+    pub fn execute(&self) -> FilteredDirectory {
+        FilteredDirectory {
+           matches: self.find_matches(self.directory),
+           directory: self.directory,
+        }
+    }
+
+    //----------- private ---------//
+
+    // TODO make this faster use a thread pool or something
+    fn find_matches(&self, directory: &Directory) -> Vec<String> {
+        let mut matches = vec![];
+        if self.is_match(&directory.path, self.filter_string) {
+            matches.extend(self.directory.contents());
+        } else {
+            for file in self.directory.files.clone() {
+                if self.is_match(&file.path(), self.filter_string) {
+                    matches.push(file.as_string());
+                }
+            }
+            for directory in self.directory.sub_directories.clone() {
+                matches.extend(self.find_matches(&directory))
+            }
+        }
+        matches
+    }
+
+    fn is_match(&self, path: &PathBuf, filter_string: &'a str) -> bool {
+        let regex = Regex::new(filter_string).unwrap();
+        regex.is_match(path.to_str().unwrap())
+    }
+
+}
+
+#[test]
+fn it_works() {
+    let mut scanner_builder = ScannerBuilder::new();
+    let scanner_builder = scanner_builder.start_from_path("test/fixture_dir/");
+    let directory = scanner_builder.build().scan();
+
+    let filter = SimpleFilter::new(&directory, "fixture_dir");
+    let filtered_directory = filter.execute();
+
+    assert_eq!(filtered_directory.len(), 10);
+}
+
+//// step 1 - simple one time, static data filtering with the output returned
+
+//let filter = OneTimeFilter::new(&result_set, &filter_string);
+//let results = filter.start();
+
+//// step 2 - dynamic data that subscribes and publishes events
+
+//let filter = ContinuousFilter::new(event_bus) // subscribes to new directory item event
+                                              //// subscribes to filter string change events
+                                              //// publishes events when a new directory item matches
+                                              //// the filter string
+
+//directory_filter.start();
+
+//// it updates the listeners as new results match the filter string
+//// it resets when the filter string is updated
+
+//directory_filter.stop();
