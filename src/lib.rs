71baae3c19a04bca0b28b0a0e292db3811795467
Seb Glazebrook <sebastian.glazebrook@redbubble.com>
2016-03-19 22:43:19 +1100

    Exporting the ScannerBuilder and Directory

--- src/lib.rs
+++ src/lib.rs
@@ -5,3 +5,4 @@ extern crate crossbeam;
 
 mod directory_filter;
 pub use directory_filter::{SimpleFilter, ContinuousFilter,FilteredDirectory};
+pub use directory_scanner::{ScannerBuilder, Directory};
