e39f46febc32af661f9b8b299208d605ee74edac
Seb Glazebrook <sebastian.glazebrook@redbubble.com>
2016-03-31 23:59:17 +1100

    Deleted old String matches

--- src/directory_filter/filtered_directory.rs
+++ src/directory_filter/filtered_directory.rs
@@ -6,7 +6,6 @@ use directory_filter::matchers::*;
 #[derive(Clone)]
 pub struct FilteredDirectory<'a> {
     directory: Arc<Mutex<Directory>>,
-    pub matches: Vec<String>, // TODO this should be a collection of references/pointers to paths in the directory
     regex: Regex, // TODO gonna need to store the filterstring/regex here
     pub match_references: Vec<&'a File>, // TODO make this actually work
     pub file_matches: Vec<File>,
@@ -16,7 +15,6 @@ impl<'a> FilteredDirectory<'a> {
 
     pub fn new(directory: Arc<Mutex<Directory>>, regex: Regex) -> Self {
       FilteredDirectory {
-           matches: vec![],
            directory: directory,
            regex: regex,
            match_references: vec![],
@@ -25,19 +23,14 @@ impl<'a> FilteredDirectory<'a> {
     }
 
     pub fn len(&self) -> usize {
-        self.matches.len()
+        self.file_matches.len()
     }
 
     pub fn run_filter(&mut self) {
         if self.regex.to_string() == "" {
-            self.matches = self.directory.lock().unwrap().flatten();
             self.file_matches = self.directory.lock().unwrap().file_contents();
         } else {
-            let results = find_matches(&self.directory, self.regex.clone()); // TODO don't return matches update the FilteredDirectory??
-            let new_matches = results.0;
-            let new_file_matches = results.1;
-            self.matches = new_matches;
-            self.file_matches = new_file_matches;
+            self.file_matches = find_matches(&self.directory, self.regex.clone()); // TODO don't return matches update the FilteredDirectory??
             info!("Filter found {} matches", self.len());
         }
     }
--- src/directory_filter/matchers.rs
+++ src/directory_filter/matchers.rs
@@ -6,30 +6,17 @@ use std::sync::atomic::{AtomicUsize, Ordering};
 use std::thread;
 use crossbeam::sync::SegQueue;
 
-pub fn find_matches(directory: &Arc<Mutex<Directory>>, regex: Regex) -> (Vec<String>, Vec<File>) {
+pub fn find_matches(directory: &Arc<Mutex<Directory>>, regex: Regex) -> Vec<File> {
     execute(directory, regex)
 }
 
 //----------- private -------------//
 
-fn execute(directory: &Arc<Mutex<Directory>>, regex: Regex) -> (Vec<String>, Vec<File>) {
-    let matches_queue = Arc::new(SegQueue::new());
+fn execute(directory: &Arc<Mutex<Directory>>, regex: Regex) -> Vec<File> {
     let file_matches_queue = Arc::new(SegQueue::new());
     let current_concurrency = Arc::new(AtomicUsize::new(0));
     let concurrency_limit = Arc::new(AtomicUsize::new(4));
-    fetch_matches(directory.clone(), regex.clone(), matches_queue.clone(), file_matches_queue.clone(), current_concurrency.clone(), concurrency_limit.clone());
-    let mut merged_matches = vec![];
-    let mut done = false;
-    while !done {
-        match matches_queue.try_pop() {
-            Some(matches) => { merged_matches.extend(matches); }
-            None => {
-                if current_concurrency.load(Ordering::Relaxed) == 0 {
-                    done = true
-                }
-            }
-        }
-    }
+    fetch_matches(directory.clone(), regex.clone(), file_matches_queue.clone(), current_concurrency.clone(), concurrency_limit.clone());
     let mut file_merged_matches = vec![];
     let mut done = false;
     while !done {
@@ -42,35 +29,32 @@ fn execute(directory: &Arc<Mutex<Directory>>, regex: Regex) -> (Vec<String>, Vec
             }
         }
     }
-    (merged_matches, file_merged_matches)
+    file_merged_matches
 }
 
 
-fn fetch_matches(directory: Arc<Mutex<Directory>>, regex: Regex, matches_queue: Arc<SegQueue<Vec<String>>>, file_matches_queue: Arc<SegQueue<Vec<File>>>, current_concurrency: Arc<AtomicUsize>, concurrency_limit: Arc<AtomicUsize>) {
+fn fetch_matches(directory: Arc<Mutex<Directory>>, regex: Regex, file_matches_queue: Arc<SegQueue<Vec<File>>>, current_concurrency: Arc<AtomicUsize>, concurrency_limit: Arc<AtomicUsize>) {
     let locked_directory = directory.lock().unwrap();
     if is_match(&locked_directory.path, &regex) {
-        matches_queue.push(locked_directory.contents());
         file_matches_queue.push(locked_directory.files.clone());
     } else {
         for file in locked_directory.files.clone() {
             if is_match(&file.path(), &regex) {
-                matches_queue.push(vec![file.as_string()]);
                 file_matches_queue.push(vec![file.clone()]);
             }
         }
         for sub_directory in locked_directory.sub_directories.clone() {
             if max_concurrency_reached(current_concurrency.clone(), concurrency_limit.clone()) {
-                fetch_matches(Arc::new(Mutex::new(sub_directory)), regex.clone(), matches_queue.clone(), file_matches_queue.clone(), current_concurrency.clone(), concurrency_limit.clone());
+                fetch_matches(Arc::new(Mutex::new(sub_directory)), regex.clone(), file_matches_queue.clone(), current_concurrency.clone(), concurrency_limit.clone());
             } else {
                 let local_current_concurrency = current_concurrency.clone();
                 let local_concurrency_limit = concurrency_limit.clone();
                 let local_regex = regex.clone();
-                let local_matches_queue = matches_queue.clone();
                 let local_file_matches_queue = file_matches_queue.clone();
                 thread::spawn(move || {
                     local_current_concurrency.fetch_add(1, Ordering::SeqCst);
                     info!("Increased filter concurrency to {:?}", local_current_concurrency.load(Ordering::Relaxed));
-                    fetch_matches(Arc::new(Mutex::new(sub_directory)), local_regex, local_matches_queue, local_file_matches_queue, local_current_concurrency.clone(), local_concurrency_limit);
+                    fetch_matches(Arc::new(Mutex::new(sub_directory)), local_regex, local_file_matches_queue, local_current_concurrency.clone(), local_concurrency_limit);
                     local_current_concurrency.fetch_sub(1, Ordering::SeqCst);
                     info!("Decreased filter concurrency to {:?}", local_current_concurrency.load(Ordering::Relaxed));
                 });
