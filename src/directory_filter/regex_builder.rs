5f6973f4807da5e7fbe18f025b0ac88995880aa5
Seb Glazebrook <sebastian.glazebrook@redbubble.com>
2016-04-04 12:02:37 +1000

    Case insensitive be default unless there is one or more uppcase in filter string

--- src/directory_filter/regex_builder.rs
+++ src/directory_filter/regex_builder.rs
@@ -12,12 +12,23 @@ impl RegexBuilder {
 
     pub fn build(&self) -> Regex {
         let mut new_string = self.string.chars().fold(String::new(), |mut acc, character|{
+            acc.push_str(self.global_flag());
             acc.push_str(".*");
             acc.push(character);
             acc
         });
         new_string.push_str(".*");
         Regex::new(&new_string).unwrap()
+    }
+
+    //----------- private -----------//
+
+    fn global_flag(&self) -> &'static str {
+        let mut prefix = "(?i)";
+        if Regex::new("[A-Z]+").unwrap().is_match(&self.string) {
+            prefix = "";
+        }
+        prefix
     }
 
 }
